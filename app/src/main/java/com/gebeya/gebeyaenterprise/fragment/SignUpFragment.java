73739503a047500295d6afa5e6a8d9759e13bc57
daveScof <davescofab@gmail.com>
2017-01-29 01:43:37 +0300

    Implement OnClickListner by ClientSignUpActivity class
    This will help neatly organzie OnClickEvents from the only two buttons in this activity

--- app/src/main/java/com/gebeya/gebeyaenterprise/fragment/SignUpFragment.java
+++ app/src/main/java/com/gebeya/gebeyaenterprise/fragment/SignUpFragment.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.gebeya.gebeyaenterprise.activity.ClientSignUpActivity;
+import com.gebeya.gebeyaenterprise.activity.TalentSignupActivity;
 import com.gebeya.gebeyaenterprise.base.BaseScreen;
 import com.gebeya.gebeyaenterprise.R;
 import com.gebeya.gebeyaenterprise.utils.Fonts;
@@ -19,16 +20,16 @@ import com.gebeya.gebeyaenterprise.utils.Fonts;
  * Created by gebeya-admin on 1/23/17.
  */
 
-public class SignUpFragment extends BaseScreen {
+public class SignUpFragment extends BaseScreen implements View.OnClickListener{
 
     Button mClientSignUpBt;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        root = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        mRoot = inflater.inflate(R.layout.fragment_sign_up, container, false);
         initViews();
         setListeners();
-        return root;
+        return mRoot;
     }
 
     @Override
@@ -42,8 +43,7 @@ public class SignUpFragment extends BaseScreen {
         mClientSignUpBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), ClientSignUpActivity.class);
-                startActivity(intent);
+
             }
         });
     }
@@ -51,6 +51,21 @@ public class SignUpFragment extends BaseScreen {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.signUp_client_bt:
+                intent = new Intent(getActivity(), ClientSignUpActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.signUp_talent_bt:
+                intent = new Intent(getActivity(), TalentSignupActivity.class);
+                startActivity(intent);
+        }
+
     }
 }
 
