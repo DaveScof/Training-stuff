e6eac17b8a38fd0ea0eaa68548b6d658b5e49197
daveScof <davescofab@gmail.com>
2017-01-29 01:33:36 +0300

    Add Application Class
    This class will be a Global object to store most common data

--- app/src/main/java/com/gebeya/gebeyaenterprise/App.java
+++ app/src/main/java/com/gebeya/gebeyaenterprise/App.java
@@ -1,8 +1,17 @@
 package com.gebeya.gebeyaenterprise;
 
+import android.app.Application;
+
+import com.gebeya.gebeyaenterprise.utils.Fonts;
+
 /**
  * Created by dave-5cof on 1/27/17.
  */
 
-public class App {
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fonts.initialize(getApplicationContext());
+    }
 }
