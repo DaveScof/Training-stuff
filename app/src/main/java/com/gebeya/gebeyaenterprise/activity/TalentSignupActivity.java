4201d650dc8352bd51a7e228f665053f3a308b54
daveScof <davescofab@gmail.com>
2017-01-29 01:34:07 +0300

    Add TalentSignUp Activity

--- app/src/main/java/com/gebeya/gebeyaenterprise/activity/TalentSignupActivity.java
+++ app/src/main/java/com/gebeya/gebeyaenterprise/activity/TalentSignupActivity.java
@@ -1,7 +1,19 @@
 package com.gebeya.gebeyaenterprise.activity;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import com.gebeya.gebeyaenterprise.R;
+import com.gebeya.gebeyaenterprise.base.BaseActivity;
+import com.gebeya.gebeyaenterprise.base.BaseSingleFragmentActivity;
+
 /**
  * Created by dave-5cof on 1/29/17.
  */
-public class TalentSignupActivity {
+public class TalentSignupActivity extends BaseSingleFragmentActivity{
+
+    @Override
+    public Fragment createFragment() {
+        return null;
+    }
 }
