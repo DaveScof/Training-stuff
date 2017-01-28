d6a64a007aed20a65f1774937103ca36233c4cca
daveScof <davescofab@gmail.com>
2017-01-29 01:44:20 +0300

    Add a Country Model
    This will hold a country name and all the cities in that country extracted form a JSON file

--- app/src/main/java/com/gebeya/gebeyaenterprise/model/Country.java
+++ app/src/main/java/com/gebeya/gebeyaenterprise/model/Country.java
@@ -1,8 +1,28 @@
 package com.gebeya.gebeyaenterprise.model;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by dave-5cof on 1/27/17.
  */
 
 public class Country {
+
+    private static ArrayList<Country> countries;
+
+    public static void addCountries(ArrayList<Country> newCountries) {
+        if (countries == null) {
+            countries = new ArrayList<>();
+        }
+        if (countries.isEmpty()) {
+            countries.addAll(newCountries);
+        }
+    }
+
+    public ArrayList<String> cities;
+
+    public Country() {
+        cities = new ArrayList<>();
+    }
 }
