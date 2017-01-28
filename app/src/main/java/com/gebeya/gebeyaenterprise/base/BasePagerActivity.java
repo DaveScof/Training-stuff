b4c2645fb3a7349282ffb75961d8ac8e346b8c85
daveScof <davescofab@gmail.com>
2017-01-29 01:34:59 +0300

    Add base BasePagerActivity
    This class will be the base for any activity that will hold one full view pager

--- app/src/main/java/com/gebeya/gebeyaenterprise/base/BasePagerActivity.java
+++ app/src/main/java/com/gebeya/gebeyaenterprise/base/BasePagerActivity.java
@@ -1,10 +1,21 @@
 package com.gebeya.gebeyaenterprise.base;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+
+import com.gebeya.gebeyaenterprise.R;
 
 /**
  * Created by dave-5cof on 1/29/17.
  */
 
 public class BasePagerActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_pager);
+
+
+    }
 }
