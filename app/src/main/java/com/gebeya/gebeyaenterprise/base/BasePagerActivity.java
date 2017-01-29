bf3acaeac885b96d57ed43e8f7e5d211c9708122
daveScof <davescofab@gmail.com>
2017-01-29 03:07:33 +0300

    Implement BasePagerActivity
    Allows simple hosting of a viewPager

--- app/src/main/java/com/gebeya/gebeyaenterprise/base/BasePagerActivity.java
+++ app/src/main/java/com/gebeya/gebeyaenterprise/base/BasePagerActivity.java
@@ -2,20 +2,45 @@ package com.gebeya.gebeyaenterprise.base;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 
-import com.gebeya.gebeyaenterprise.R;
 
 /**
  * Created by dave-5cof on 1/29/17.
  */
 
-public class BasePagerActivity extends FragmentActivity {
+public abstract class BasePagerActivity extends FragmentActivity {
+
+    public ViewPager mViewPager;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.view_pager);
+    }
 
+    public ViewPager getVp (int id) {
+        return  (ViewPager) findViewById(id);
+    }
 
+    public boolean setUpViewPager (final int count, final Fragment... fragments){
+        if (mViewPager == null)
+            return false;
+
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return fragments[position];
+            }
+
+            @Override
+            public int getCount() {
+                return count;
+            }
+        });
+
+        return true;
     }
 }
