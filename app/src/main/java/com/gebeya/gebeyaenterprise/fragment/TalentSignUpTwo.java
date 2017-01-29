e1b0a77587466848ad1608393bd96eeeb1fd890c
daveScof <davescofab@gmail.com>
2017-01-29 03:53:56 +0300

    Change Language Selection Spinner to a Button

--- app/src/main/java/com/gebeya/gebeyaenterprise/fragment/TalentSignUpTwo.java
+++ app/src/main/java/com/gebeya/gebeyaenterprise/fragment/TalentSignUpTwo.java
@@ -2,34 +2,47 @@ package com.gebeya.gebeyaenterprise.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.gebeya.gebeyaenterprise.R;
 import com.gebeya.gebeyaenterprise.base.BaseScreen;
+import com.gebeya.gebeyaenterprise.dialog.SkillsDialog;
 
 /**
  * Created by dave-5cof on 1/29/17.
  */
 
 public class TalentSignUpTwo extends BaseScreen {
+    private static final String DIALOG_SKILL = "DialogSkill";
+    private Button mLangButton;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mRoot = inflater.inflate(R.layout.talent_signup2, container, false);
-
+        initViews();
+        setListeners();
         return mRoot;
     }
 
     @Override
     protected void initViews() {
-
+        mLangButton = getBt(R.id.langs_spinner);
     }
 
     @Override
     protected void setListeners() {
-
+        mLangButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fg = getFragmentManager();
+                SkillsDialog dialog = new SkillsDialog();
+                dialog.show(fg,DIALOG_SKILL);
+            }
+        });
     }
 }
