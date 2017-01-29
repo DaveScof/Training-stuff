154e7c60693a854525a9d304deaa271e804de78c
daveScof <davescofab@gmail.com>
2017-01-29 03:07:04 +0300

    Add two TalentSignUp fragments into viewPager

--- app/src/main/java/com/gebeya/gebeyaenterprise/activity/TalentSignupActivity.java
+++ app/src/main/java/com/gebeya/gebeyaenterprise/activity/TalentSignupActivity.java
@@ -1,19 +1,26 @@
 package com.gebeya.gebeyaenterprise.activity;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
 import com.gebeya.gebeyaenterprise.R;
-import com.gebeya.gebeyaenterprise.base.BaseActivity;
-import com.gebeya.gebeyaenterprise.base.BaseSingleFragmentActivity;
+import com.gebeya.gebeyaenterprise.base.BasePagerActivity;
+import com.gebeya.gebeyaenterprise.fragment.TalentSignUpOne;
+import com.gebeya.gebeyaenterprise.fragment.TalentSignUpTwo;
+
 
 /**
  * Created by dave-5cof on 1/29/17.
  */
-public class TalentSignupActivity extends BaseSingleFragmentActivity{
-
+public class TalentSignupActivity extends BasePagerActivity{
     @Override
-    public Fragment createFragment() {
-        return null;
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.talent_signup_view_pager);
+
+        mViewPager = getVp(R.id.singleViewPager);
+        Fragment[] fragments = new Fragment[]{new TalentSignUpOne(), new TalentSignUpTwo()};
+        setUpViewPager(fragments.length, fragments);
     }
 }
