cca0811dff61151faaee83f9d5c3069fe7b71ea4
daveScof <davescofab@gmail.com>
2017-01-29 03:51:56 +0300

    Add Skills Dialog and Base Dialog

---
+++ app/src/main/java/com/gebeya/gebeyaenterprise/base/BaseDialog.java
@@ -0,0 +1,10 @@
+package com.gebeya.gebeyaenterprise.base;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by dave-5cof on 1/29/17.
+ */
+
+public class BaseDialog extends DialogFragment {
+}
---
+++ app/src/main/java/com/gebeya/gebeyaenterprise/dialog/SkillsDialog.java
@@ -0,0 +1,8 @@
+package com.gebeya.gebeyaenterprise.dialog;
+
+/**
+ * Created by dave-5cof on 1/29/17.
+ */
+
+public class SkillsDialog {
+}
