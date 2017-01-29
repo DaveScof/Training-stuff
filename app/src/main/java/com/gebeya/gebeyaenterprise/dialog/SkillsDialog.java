80d535583ecc3da6e94058c34989cc6c23b282f1
daveScof <davescofab@gmail.com>
2017-01-29 03:53:14 +0300

    Create a Skills Alert Dialog

--- app/src/main/java/com/gebeya/gebeyaenterprise/dialog/SkillsDialog.java
+++ app/src/main/java/com/gebeya/gebeyaenterprise/dialog/SkillsDialog.java
@@ -1,8 +1,30 @@
 package com.gebeya.gebeyaenterprise.dialog;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.gebeya.gebeyaenterprise.R;
+import com.gebeya.gebeyaenterprise.base.BaseDialog;
+
 /**
  * Created by dave-5cof on 1/29/17.
  */
 
-public class SkillsDialog {
+public class SkillsDialog extends BaseDialog {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_skills, null);
+
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.select_languages)
+                .setPositiveButton(android.R.string.ok,null)
+                .setView(view)
+                .create();
+    }
 }
