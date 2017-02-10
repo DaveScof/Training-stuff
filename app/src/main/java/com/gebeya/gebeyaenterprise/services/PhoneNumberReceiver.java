5f2d800ad1579c360e36090c46aba43c99cc9789
daveScof <davescofab@gmail.com>
2017-02-10 20:22:32 +0300

    Add Utils

---
+++ app/src/main/java/com/gebeya/gebeyaenterprise/services/PhoneNumberReceiver.java
@@ -0,0 +1,10 @@
+package com.gebeya.gebeyaenterprise.services;
+
+import android.content.BroadcastReceiver;
+
+/**
+ * Created by davescof on 2/10/17.
+ */
+
+public class PhoneNumberReceiver extends BroadcastReceiver {
+}
---
+++ app/src/main/java/com/gebeya/gebeyaenterprise/utils/Util.java
@@ -0,0 +1,8 @@
+package com.gebeya.gebeyaenterprise.utils;
+
+/**
+ * Created by davescof on 2/10/17.
+ */
+
+public class Util {
+}
