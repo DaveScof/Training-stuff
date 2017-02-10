3828e3836c1d5527c32361585f79e8f69f00c16b
daveScof <davescofab@gmail.com>
2017-02-11 00:08:23 +0300

    Add HomeScreen Pager Fragments

---
+++ app/src/main/java/com/gebeya/gebeyaenterprise/activity/HomePagerActivity.java
@@ -0,0 +1,15 @@
+package com.gebeya.gebeyaenterprise.activity;
+
+import com.gebeya.gebeyaenterprise.R;
+import com.gebeya.gebeyaenterprise.base.BasePagerActivity;
+
+/**
+ * Created by davescof on 2/11/17.
+ */
+
+public class HomePagerActivity extends BasePagerActivity {
+    @Override
+    protected void setContentView() {
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/gebeya/gebeyaenterprise/fragment/TalentPagerFragment.java
@@ -0,0 +1,19 @@
+package com.gebeya.gebeyaenterprise.fragment;
+
+import com.gebeya.gebeyaenterprise.base.BaseScreen;
+
+/**
+ * Created by davescof on 2/11/17.
+ */
+
+public class TalentPagerFragment extends BaseScreen {
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void setListeners() {
+
+    }
+}
